31650058334f962acac2d3636e3c243bbbcf3283
kiieeaa <kiyaadinda5@gmail.com>
2025-04-29 18:31:56 +0700

    membuat procedur printdata

--- intoduceOOP.cpp
+++ intoduceOOP.cpp
@@ -7,6 +7,12 @@ class Mahasiswa{
     string nama;
     float nilai;
 
+    void printData(){
+        cout << "Nim :" << nim << endl;
+        cout << "Nama :" << nama << endl;
+        cout << "Nilai :" << nilai << endl;
+    }
+
 };
 int main(){
 
